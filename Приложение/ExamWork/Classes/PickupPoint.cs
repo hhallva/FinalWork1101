594002429b09aa266afcae8001f758634c675925
Вячеслав <slavvva05@gmail.com>
2024-12-17 16:12:05 +0300

    Add class PickupPoint.cs

---
+++ Приложение/ExamWork/Classes/PickupPoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam
+{
+    public class PickupPoint
+    {
+        public int PickupPointIndex { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Buliding { get; set; }
+    }
+}
