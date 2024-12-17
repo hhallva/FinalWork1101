83fbef0a8d88e0182c18995bfbffe0a620562162
Вячеслав <slavvva05@gmail.com>
2024-12-17 19:13:12 +0300

    Update ExamUser.cs

--- Приложение/DataBaseLibrary/Models/ExamUser.cs
+++ Приложение/DataBaseLibrary/Models/ExamUser.cs
@@ -5,6 +5,7 @@ namespace DataBaseLibrary.Models;
 
 public partial class ExamUser
 {
+    #region Свойства
     public int UserId { get; set; }
 
     public byte RoleId { get; set; }
@@ -22,4 +23,19 @@ public partial class ExamUser
     public virtual ICollection<ExamOrder> ExamOrders { get; set; } = new List<ExamOrder>();
 
     public virtual ExamRole Role { get; set; } = null!;
+    #endregion
+
+    #region Констуркторы 
+    public ExamUser(string name, string patronymic, string surname, string login, string password, byte role)
+    {
+        Name = name;
+        Patronymic = patronymic;
+        Surname = surname;
+        Login = login;
+        Password = password;
+        RoleId = role;
+    }
+
+    public ExamUser() : this("Гость", string.Empty, string.Empty, string.Empty, string.Empty, 2) { }
+    #endregion
 }
