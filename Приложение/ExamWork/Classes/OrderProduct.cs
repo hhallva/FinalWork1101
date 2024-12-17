86d9f5d48feb54da934b953f45dab8da20e3263c
Вячеслав <slavvva05@gmail.com>
2024-12-17 16:15:23 +0300

    Add class OrderProduct

---
+++ Приложение/ExamWork/Classes/OrderProduct.cs
@@ -0,0 +1,11 @@
+﻿using ExamWork.Classes;
+
+namespace Exam
+{
+    public class OrderProduct
+    {
+        public Order OrderID { get; set; }
+        public Product ArticleNumber { get; set; }
+        public int Amount { get; set; }
+    }
+}
