3513bd5e23df098941530c2bd137e4965153dbe3
Вячеслав <slavvva05@gmail.com>
2024-12-17 23:18:49 +0300

    Update ProductService.cs

--- Приложение/DataBaseLibrary/Services/ProductService.cs
+++ Приложение/DataBaseLibrary/Services/ProductService.cs
@@ -1,6 +1,7 @@
 ﻿using DataBaseLibrary.Models;
 using DataBaseLibrary.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 
 namespace DataBaseLibrary.Services
 {
@@ -8,7 +9,16 @@ namespace DataBaseLibrary.Services
     {
         private readonly AddDbContext _context = new();
 
-        public async Task<List<ExamProduct>> GetProductsAsync() 
-            => await _context.ExamProducts.ToListAsync();
+        public async Task<List<ExamProduct>> GetProductsAsync(string filtersQuery)
+        {
+            return await _context.ExamProducts.ToListAsync();
+        }
+
+        public async Task<int> GetProductsCountAsync()
+        {
+            List<ExamProduct> products = await _context.ExamProducts.ToListAsync();
+            return products.Count();
+        }
+
     }
 }
