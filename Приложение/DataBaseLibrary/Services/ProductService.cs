db11ec913fc0a440ab4b2ab8a1605a43adebd561
Вячеслав <slavvva05@gmail.com>
2024-12-17 19:13:21 +0300

    Create ProductService.cs

---
+++ Приложение/DataBaseLibrary/Services/ProductService.cs
@@ -0,0 +1,14 @@
+﻿using DataBaseLibrary.Models;
+using DataBaseLibrary.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseLibrary.Services
+{
+    public class ProductService
+    {
+        private readonly AddDbContext _context = new();
+
+        public async Task<List<ExamProduct>> GetProductsAsync() 
+            => await _context.ExamProducts.ToListAsync();
+    }
+}
