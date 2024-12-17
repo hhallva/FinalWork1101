24badfd51b5df29f408cf3b36b274ca6449e62d8
Вячеслав <slavvva05@gmail.com>
2024-12-17 16:14:25 +0300

    Add class Order

---
+++ Приложение/ExamWork/Classes/Order.cs
@@ -0,0 +1,15 @@
+﻿using ExamWork.Classes;
+
+namespace Exam
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public User UserID { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public PickupPoint PickupPointIndex { get; set; }
+        public int PickupCode { get; set; }
+    }
+}
