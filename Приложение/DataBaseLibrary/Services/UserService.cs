55da8bfa181fb8f2c37ec1b11d5a688693c0787b
Вячеслав <slavvva05@gmail.com>
2024-12-17 18:10:18 +0300

    Create UserService.cs

---
+++ Приложение/DataBaseLibrary/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using DataBaseLibrary.Data;
+
+namespace DataBaseLibrary.Services
+{
+    class UserService
+    {
+        private readonly AddDbContext _context;
+
+        public UserService()
+        {
+            _context = new AddDbContext();
+        }
+
+        #region Методы
+        public void Method()
+        {
+
+        }
+
+        #endregion
+    }
+}
