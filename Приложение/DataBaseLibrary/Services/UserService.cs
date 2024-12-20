306145ec1f71fdceac144270851c5ca72625a319
Вячеслав <156709873+hhallva@users.noreply.github.com>
2024-12-20 13:13:05 +0300

    Update UserService.cs

--- Приложение/DataBaseLibrary/Services/UserService.cs
+++ Приложение/DataBaseLibrary/Services/UserService.cs
@@ -1,4 +1,4 @@
-﻿using DataBaseLibrary.Data;
+using DataBaseLibrary.Data;
 using DataBaseLibrary.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +9,7 @@ namespace DataBaseLibrary.Services
         private readonly AddDbContext _context = new();
 
         public async Task<bool> IsUserExistAsync(string login, string password)
-            => (await _context.ExamUsers.SingleOrDefaultAsync(u => u.Login == login && u.Password == password) != null) ? true : false;
+            => (await _context.ExamUsers.SingleOrDefaultAsync(u => u.Login == login && u.Password == password) != null);
 
         public async Task<ExamUser> GetUserAsync(string login, string password)
             => await _context.ExamUsers.SingleAsync(u => u.Login == login && u.Password == password);
